0dd23b17312ed8838f92cdab9faa77f798219169
jiangbing <jiangbing@ruijie.com.cn>
2019-03-29 23:10:27 +0800

    优化键盘hook效果

--- HookMsgs.cpp
+++ HookMsgs.cpp
@@ -6,14 +6,43 @@
 #include <map>
 
 using namespace std;
-
+#if 1
+#define HOOK_LOG
+#endif 
 typedef void* (*P_HOOK_PROC) (HWND, int, WPARAM, LPARAM, void *);
 
-HHOOK	g_hMouse	= NULL;
-HHOOK	g_hKeyboard	= NULL;
-FILE*	g_pFile		= NULL;
-BOOL	g_bShowed	= FALSE;
-long long g_lastEventTime = 0;
+typedef enum SysKeyType {
+	NONE_KEY = 0,
+	CTRL_KEY,
+	ALT_KEY,
+	SHIFT_KEY
+};
+
+typedef enum SysKeyStat {
+	KEY_NO_STATE = 0,
+	KEY_DOWN,
+	KEY_UP
+};
+
+typedef struct SysKeyMark {
+	BOOL		bAlt;
+	BOOL		bShift;
+	BOOL		bCtrl;
+	SysKeyStat	state;
+	long long	keyupTime;
+};
+
+#define EVENT_EXPIRE_TIME = 500
+
+HHOOK	g_hMouse		= NULL;
+HHOOK	g_hKeyboard		= NULL;
+FILE*	g_pFile			= NULL;
+BOOL	g_bShowed		= FALSE;
+
+long long g_lastEventTime	= 0;
+long long g_eventExpireTime = 2000;
+
+SysKeyMark g_sysKeyMark = {0};
 
 #define MAP		map<UINT, P_HOOK_PROC>
 #define PAIR	pair<UINT, P_HOOK_PROC>
@@ -50,8 +79,6 @@ LRESULT CALLBACK MouseProc(
 	LPARAM lParam   // mouse coordinates
 	)
 {
-
-
 #ifdef HOOK_LOG
 	if (g_pFile != NULL) {
 		fprintf(g_pFile, "[Message] code: %d, wParam: %d, lParam: %d.\n", nCode, wParam, lParam);
@@ -82,7 +109,7 @@ LRESULT CALLBACK KeyboardProc(
 {
 #ifdef HOOK_LOG
 	if (g_pFile != NULL) {
-		fprintf(g_pFile, "[Message] code: %d, wParam: %d, lParam: %d.\n", nCode, wParam, lParam);
+		fprintf(g_pFile, "[Message] code: %d, wParam: %d, low: %c, lParam: %d.\n", nCode, wParam, (char)wParam, lParam);
 		fflush(g_pFile);
 	}
 #endif
@@ -99,56 +126,81 @@ LRESULT CALLBACK KeyboardProc(
 #else
 //	g_hookProc(g_hWnd, nCode, wParam, lParam, NULL);
 #endif
+	LRESULT result = CallNextHookEx(g_hKeyboard, nCode, wParam, lParam);
+	long long curTimestamp = GetTickCount();
+	long long interval = curTimestamp - g_lastEventTime;
+	g_lastEventTime = curTimestamp;
 
-	if (HC_ACTION == nCode) {
-		if (WM_KEYDOWN == wParam || WM_SYSKEYDOWN) {
-			BOOL bCtrl	= GetKeyState(VK_CONTROL)	& 0x8000;  
-			BOOL bShift	= GetKeyState(VK_SHIFT)		& 0x8000;  
-			BOOL bAlt	= GetKeyState(VK_MENU)		& 0x8000; 
-			if (((char)wParam == 'C' || (char)wParam == 'c') && bShift) {
-				if ((lParam & 0x40000000) != 0) { //响应按键按下，按键抬起则略过。
-					if (g_bShowed) {
-						ShowWindow(g_hWnd, SW_HIDE);
-						g_bShowed = FALSE;
-					} else {
-						::SetWindowPos(g_hWnd, HWND_TOP, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE); 
-						ShowWindow(g_hWnd, SW_SHOW);
-						::SetWindowPos(g_hWnd, HWND_TOP, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE); 
-						g_bShowed = TRUE;
-					}
+	if ((WM_KEYDOWN == wParam || WM_SYSKEYDOWN)) {
+		if ((lParam & 0x40000000) != 0) {
+
+			g_sysKeyMark.bAlt	= GetKeyState(VK_MENU)	  & 0x8000;
+			g_sysKeyMark.bCtrl	= GetKeyState(VK_CONTROL) & 0x8000;
+			g_sysKeyMark.bShift = GetKeyState(VK_SHIFT)   & 0x8000;
+
+			if (g_sysKeyMark.bAlt || g_sysKeyMark.bCtrl || g_sysKeyMark.bShift) {
+				g_sysKeyMark.state = KEY_DOWN;
+				g_sysKeyMark.keyupTime = 0;
+			}
+			
+			if (g_sysKeyMark.state == KEY_UP && interval >= g_eventExpireTime) {
+				memset(&g_sysKeyMark, 0x00, sizeof(SysKeyMark));
+			}
+			interval = curTimestamp - g_sysKeyMark.keyupTime;
+
+			if (g_sysKeyMark.bAlt && (g_sysKeyMark.state == KEY_DOWN || (g_sysKeyMark.state == KEY_UP && interval < g_eventExpireTime)) && ((char)wParam == '1')) {
+				if (g_bShowed) {
+					ShowWindow(g_hWnd, SW_HIDE);
+					g_bShowed = FALSE;
+
+				} else {
+					//MessageBox(g_hWnd, "SHOW COMMAND", "", NULL);
+					ShowWindow(g_hWnd, SW_SHOW);
+					SetWindowPos(g_hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE); 
+					g_bShowed = TRUE;
 				}
 				return 1;
-			} else if (wParam == VK_DOWN) {
-				if ((lParam & 0x40000000) != 0) {
-				//	SendMessage(g_hWnd, WM_USER + 101, NULL, NULL);
-				}
-				return 1;
-			} else if (wParam == VK_UP) {
-				if ((lParam & 0x40000000) != 0) {
-				//	SendMessage(g_hWnd, WM_USER + 102, NULL, NULL);
-				}
-				return 1;
-			} else if (((char)wParam == 'G' || (char)wParam == 'g') && bShift) {
+			} else if (g_sysKeyMark.bAlt && (g_sysKeyMark.state == KEY_DOWN || (g_sysKeyMark.state == KEY_UP && interval < g_eventExpireTime)) && ((char)wParam == '2')) {
 				if ((lParam & 0x40000000) != 0) {
 					SendMessage(g_hWnd, WM_USER + 103, NULL, NULL);
 				}
 				return 1;
-			} else if (((char)wParam == 'D' || (char)wParam == 'd') && bShift) {
+			} else if (g_sysKeyMark.bAlt && (g_sysKeyMark.state == KEY_DOWN || (g_sysKeyMark.state == KEY_UP && interval < g_eventExpireTime)) && ((char)wParam == '3')) {
 				if ((lParam & 0x40000000) != 0) {
 					SendMessage(g_hWnd, WM_USER + 104, NULL, NULL);
 				}
 				return 1;
 			}
+		} else {
+			if (!(GetKeyState(VK_CONTROL) & 0x8000) 
+				&& !(GetKeyState(VK_SHIFT) & 0x8000)
+				&& !(GetKeyState(VK_MENU) & 0x8000)) {
+				if (g_sysKeyMark.bAlt || g_sysKeyMark.bCtrl || g_sysKeyMark.bShift) {
+					g_sysKeyMark.state = KEY_UP;
+					g_sysKeyMark.keyupTime = curTimestamp;
+				}
+			}
 		}
+
+	} else {
+
 	}
-	return CallNextHookEx(g_hKeyboard, nCode, wParam, lParam);
+
+#ifdef HOOK_LOG
+	if (g_pFile != NULL) {
+		fprintf(g_pFile, "[message] balt=%d,bctrl=%d,bshift=%d,keystat=%d,uptime=%I64d,interval=%I64d\n", g_sysKeyMark.bAlt,g_sysKeyMark.bCtrl,g_sysKeyMark.bShift,g_sysKeyMark.state,g_sysKeyMark.keyupTime,interval);
+		fflush(g_pFile);
+	}
+#endif
+	g_lastEventTime = curTimestamp;
+	return result;
 }
 
 void SetHook(HWND hwnd)
 {
 	g_hWnd=hwnd;
-	g_hMouse=SetWindowsHookEx(WH_MOUSE,MouseProc,GetModuleHandle("HookMsgs"),0);
-	g_hKeyboard=SetWindowsHookEx(WH_KEYBOARD,KeyboardProc,GetModuleHandle("HookMsgs"),0);
+//	g_hMouse=SetWindowsHookEx(WH_MOUSE,MouseProc,GetModuleHandle("HookMsgs"),0);
+	g_hKeyboard=SetWindowsHookExA(WH_KEYBOARD, KeyboardProc, GetModuleHandle("HookMsgs"), 0);
 
 #ifdef HOOK_LOG
 	g_pFile = fopen("hook.log", "a+");
